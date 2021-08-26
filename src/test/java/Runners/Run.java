ee795caa57b7d0cea9635a8159f00d41ac5ca17b
sharonvofox <sharon.g@vofoxsolutions.com>
2021-08-26 18:47:56 +0530

    different format reports

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -7,8 +7,14 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/GetPost.feature",
         glue = {"StepDefinition"},
         dryRun = false,
-        monochrome=true,
+        monochrome=false,
         strict=true,
+
+        //----Different types of report format----
+
+       // plugin = { "pretty" }
+        //plugin = { "usage" }
+        //plugin = { "pretty", "html:target/cucumber-reports" }
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class Run extends AbstractTestNGCucumberTests {
